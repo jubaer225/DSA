1c9797a64c54573490fdb8873d1c967ac8159721
Md. Jubayer Hossain <jubayer15-919@diu.edu.bd>
2026-05-11 22:36:30 +0600

    Time: 6 ms (50.36%) | Memory: 55.8 MB (94.77%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    if (s.length === 0) return true;
+
+    let i = 0;
+    let j = s.length - 1;
+
+    while (i < j) {
+        if(s[i] !== s[j]) {
+            return false;
+        } 
+        i ++;
+        j --;
+    }
+
+    return true;
+};
